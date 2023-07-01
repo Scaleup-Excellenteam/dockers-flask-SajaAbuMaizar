bd108d53ae10afc77c6c86c2efd7fff0269e0cfe
SajaAbuMaizar <sajaabu@edu.hac.ac.il>
2023-07-01 22:03:10 +0300

    added all the program files and code

---
+++ dart-executer/dart-executer.py
@@ -0,0 +1,18 @@
+﻿from flask import Flask, request, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+
+@app.route('/execute', methods=['POST'])
+def execute_code():
+    code = request.files['file']
+    code.save('main.dart')
+    result = subprocess.run(['dart', 'main.dart'], capture_output=True)
+    output = result.stdout.decode()
+    error = result.stderr.decode()
+    return jsonify({'output': output, 'error': error})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5003)
---
+++ router/roueter.py
@@ -0,0 +1,43 @@
+﻿from flask import Flask, request, jsonify
+import os
+import subprocess
+import tempfile
+
+app = Flask(__name__)
+
+
+@app.route('/code', methods=['POST'])
+def receive_code():
+    code = request.files['file']
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        code.save(temp_file.name)
+        code_path = temp_file.name
+    return jsonify({'message': 'Code received successfully', 'code_path': code_path})
+
+
+@app.route('/execute', methods=['GET'])
+def execute_code():
+    language = request.args.get('language')
+    code_path = request.args.get('code_path')
+    if language == 'java':
+        result = subprocess.run(['java', '-jar', '/dockers-flask-SajaAbuMaizar/java-executor/executor.jar', code_path],
+                                capture_output=True)
+        output = result.stdout.decode()
+        error = result.stderr.decode()
+    elif language == 'python':
+        result = subprocess.run(['python', '/dockers-flask-SajaAbuMaizar/python-executor/executor.py', code_path],
+                                capture_output=True)
+        output = result.stdout.decode()
+        error = result.stderr.decode()
+    elif language == 'dart':
+        result = subprocess.run(['/dockers-flask-SajaAbuMaizar/dart-executor/executor', code_path], capture_output=True)
+        output = result.stdout.decode()
+        error = result.stderr.decode()
+    else:
+        return jsonify({'error': 'Invalid language'})
+
+    return jsonify({'output': output, 'error': error})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
